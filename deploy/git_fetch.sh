0f8e84d636824ceee8033ac29ae887627ef94858
macroEagle <macroyphong@maplequad.com>
2020-01-11 21:51:29 +0800

    update deploy script

--- deploy/git_fetch.sh
+++ deploy/git_fetch.sh
@@ -1,15 +1,18 @@
-cd /mars/git
-rm -rf MARS_Sensor
-git clone https://github.com/macroEagle/MARS_Sensor.git
-if diff -aqr -x=.git /mars/git /mars/git_bak
+cd /mars/git/MARS_Sensor
+git fetch
+if git diff master origin/master
 then
-	echo "Match!"
+	echo "Match."
 else
-	echo "No MMMMatch.."
+	echo "No Match."
+	git pull
+	#Update deploy.sh into deploy folder
 	cp /mars/git/MARS_Sensor/deploy/*.sh /mars/deploy
 	cd /mars/deploy
-    chmod +x *.sh
-	cd /mars/git_bak
-	rm -rf MARS_Sensor
-	rsync -ax --exclude [.git] /mars/git/. /mars/git_bak/.
+    chmod +x *.sh	
+	#Update script and restart the service
+	cd /mars/scripts
+	rm *.*
+	cp /mars/git/MARS_Sensor/scripts/*.* /mars/scripts
+	sudo systemctl restart mars-sensor@pi
 fi
