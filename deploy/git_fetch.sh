e5739715669aab20b013d6c09e5c9e3a6384fbc7
macroEagle <macroyphong@maplequad.com>
2020-01-11 20:35:46 +0800

    update deploy

--- deploy/git_fetch.sh
+++ deploy/git_fetch.sh
@@ -1,12 +1,15 @@
 cd /mars/git
 rm -rf MARS_Sensor
 git clone https://github.com/macroEagle/MARS_Sensor.git
-if diff -aqr /mars/git /mars/git_bak
+if diff -aqr -x=.git /mars/git /mars/git_bak
 then
 	echo "Match!"
 else
 	echo "No match!"
+	cp /mars/git/MARS_Sensor/deploy/*.sh /mars/deploy
+	cd /mars/deploy
+    chmod +x *.sh
 	cd /mars/git_bak
-	rm -rf *.*
-	cp -r /mars/git/. /mars/git_bak/.
+	rm -rf MARS_Sensor
+	rsync -ax --exclude [.git] /mars/git/. /mars/git_bak/.
 fi
